304968b525933b93338838e3bb736fc820e1e57c
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2026-08-02 01:02:11 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://car-backendv1.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
